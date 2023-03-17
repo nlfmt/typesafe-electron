a5a1a7ff7efae206e996ae1fcc6da3ba6f4cad33
nlfmt <nlfmt@gmx.de>
2023-03-17 02:26:37 +0100

    add other query types for models

--- electron/db/models.ts
+++ electron/db/models.ts
@@ -3,6 +3,8 @@ import z from "zod";
 const user = z.object({
     isAdmin: z.boolean().default(true).describe("Is the user an admin?"),
     name: z.string().nonempty().describe("The user's name"),
-})
+    array: z.array(z.string()).default([]),
+    tuple: z.tuple([z.string(), z.number()]),
+});
 
-export default { user }
+export default { user } as const;
--- electron/db/schema.ts
+++ electron/db/schema.ts
@@ -82,6 +82,7 @@
 // export type DB_TYPE = string | number | boolean | object | any[] | Date;
 
 import z from "zod";
+import { v4 as uuid } from "uuid";
 
 // const MODELS = {
 //     user: {
@@ -97,29 +98,66 @@ import z from "zod";
 // }
 import MODELS from "./models";
 
+class ObjectId {
+    id: string;
+    constructor(id?: string) {
+        if (id) {
+            if (/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(id)) {
+                this.id = id;
+            } else throw new Error("Invalid ObjectId");
+        } else {
+            this.id = uuid();
+        }
+    }
+
+    toString() {
+        return this.id;
+    }
+
+}
+
+
 type MODEL_NAMES = keyof typeof MODELS;
 type MODELS_T = {
-    [K in MODEL_NAMES]: z.infer<typeof MODELS[K]>
+    [K in MODEL_NAMES]: z.infer<typeof MODELS[K]> & { _id: ObjectId }
 };
 type DBType = {
     [K in MODEL_NAMES]: {
-        find: (query: Partial<MODELS_T[K]>) => MODELS_T[K] | null,
+        findById: (id: ObjectId) => MODELS_T[K] | null,
+        find: (query: Partial<FindQuery<MODELS_T[K]>>) => MODELS_T[K] | null,
+        findMany: (query: Partial<FindQuery<MODELS_T[K]>>) => MODELS_T[K][],
+        insert: (obj: Omit<MODELS_T[K], "_id">) => MODELS_T[K] | null,
     }
+}
+
+type FindQuery<T> = {
+    [K in keyof T]: T[K] extends any[] ? T[K] | { $contains: T[K][number] } | { $is: T[K][number] } : T[K]
 }
 
 const db = new Proxy({}, {
     get(_, prop) {
         return {
             find: find.bind({ model: prop as keyof typeof MODELS }),
+            insert: insert.bind({ model: prop as keyof typeof MODELS }),
         }
     }
 }) as DBType;
 
+const usr = db.user.find({ isAdmin: true, tuple: { $contains: "test" } })
+db.user.insert({ isAdmin: true, name: "test", array: ["test"], tuple: ["test", 1] })
 
 function find(this: { model: MODEL_NAMES }, query: object): object | null {
     const { model } = this;
     const modelSchema = MODELS[model];
     const user = modelSchema.parse(query);
+
+    return null;
+}
+
+function insert(this: { model: MODEL_NAMES }, obj: object): object | null {
+    const { model } = this;
+    const modelSchema = MODELS[model];
+    const user = modelSchema.parse(obj);
 
     return null;
 }
