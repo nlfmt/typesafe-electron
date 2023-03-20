4fd13c7b531725e995de0f4240e0530285a9c946
nlfmt <nlfmt@gmx.de>
2023-03-20 17:13:48 +0100

    remove old code from main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -2,23 +2,10 @@ import { app } from "electron";
 import "./bridge/security";
 import { createWindow } from "./window";
 
-import db, { Test } from "./db";
+import db from "./db";
 
 app.whenReady().then(createWindow);
 
 db.$ready.then(() => {
-    db.test.create({
-        name: "test",
-        array: [new Test("test", 1), new Test("test2", 2)],
-        nested: {
-            test: new Test("test3", 3)
-        },
-        test: new Test("test4", 4)
-    });
-    console.log(db.test.findMany({}).forEach(t => {
-        console.log(t);
-        t.test.test();
-        t.array.forEach(t => t.test());
-        t.nested.test.test();
-    }));
+    console.log("All Users", db.user.findMany());
 });
