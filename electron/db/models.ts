85d922b8a4c4ca6b4cea112886a2a995d83abab2
nlfmt <nlfmt@gmx.de>
2023-03-26 22:31:17 +0200

    improve db example code

--- electron/db/models.ts
+++ electron/db/models.ts
@@ -1,18 +1,21 @@
 import z from "zod";
 
-export const userModel = z.object({
+const userModel = z.object({
     isAdmin: z.boolean().default(true).describe("Is the user an admin?"),
     name: z.string().nonempty().describe("The user's name"),
-    array: z.array(z.string()).default([]),
-    tuple: z.tuple([z.string(), z.number()])
 });
 export type User = z.infer<typeof userModel>;
 
-export const postModel = z.object({
+const postModel = z.object({
     title: z.string().nonempty().describe("The post's title"),
     content: z.string().nonempty().describe("The post's content"),
     data: z.object({
-        test: z.string().nonempty().describe("The post's test")
+        test: z.string().nonempty()
     })
 });
 export type Post = z.infer<typeof postModel>;
+
+export default {
+    user: userModel,
+    post: postModel
+}
--- electron/main.ts
+++ electron/main.ts
@@ -3,9 +3,17 @@ import "./bridge/security";
 import { createWindow } from "./window";
 
 import db, { User } from "./db";
+import { $neq } from "@nlfmt/stormdb";
 
 app.whenReady().then(createWindow);
 
 db.$ready.then(() => {
-    console.log("All Users", db.user.findMany());
+    const usr = db.user.create({ name: "John Doe" });
+    const allusers = db.user.findMany();
+
+    console.log("All Users", allusers);
+
+    if (allusers.length > 3) {
+        db.user.delete({ _id: $neq(usr._id) });
+    }
 });
